b000c7fb6b44ab080bc8df4bb9dd7cb656f7b289
estanislaomolina <estanislaomolina05@gmail.com>
2025-03-21 19:05:22 -0300

    funcion individual para cada instruccion

--- TP1-ARM/src/sim.c
+++ TP1-ARM/src/sim.c
@@ -6,7 +6,75 @@
 #include <stdint.h>  // Para tipos de datos como uint64_t
 #include "shell.h"   // Incluye la definición de CPU_State y otras estructuras
 
+void hlt(){
+    printf("HLT: Halting simulation.\n");
+    RUN_BIT = 0;
+}
 
+void adds_imm(uint32_t rd, uint32_t rn, uint32_t imm12){
+    NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rn] + imm12;
+}
+
+void adds_ext(uint32_t rd, uint32_t rn, uint32_t imm12){
+    NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rn] + imm12;
+}
+
+void subs_imm(uint32_t rd, uint32_t rn, uint32_t imm12){
+    NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rn] - imm12;
+}
+
+void subs_ext(uint32_t rd, uint32_t rn, uint32_t imm12){
+    NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rn] - imm12;
+}
+
+void ands(uint32_t rd, uint32_t rn, uint32_t rm){
+    NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rn] & CURRENT_STATE.REGS[rm];
+}
+
+void beq(uint32_t imm26){
+    if (CURRENT_STATE.FLAG_Z == 1) {
+        NEXT_STATE.PC = CURRENT_STATE.PC + imm26 * 4;
+    }
+}
+
+void eors(uint32_t rd, uint32_t rn, uint32_t rm){
+    NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rn] ^ CURRENT_STATE.REGS[rm];
+}
+
+void movz(uint32_t rd, uint32_t imm16){
+    NEXT_STATE.REGS[rd] = imm16;
+}
+
+void lsl(uint32_t rd, uint32_t rn, uint32_t shamt){
+    NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rn] << shamt;
+}
+
+void stur(uint32_t rt, uint32_t rn, uint32_t imm12){
+    mem_write_32(CURRENT_STATE.REGS[rn] + imm12, CURRENT_STATE.REGS[rt]);
+}
+
+void sturb(uint32_t rt, uint32_t rn, uint32_t imm12){
+    // Adaptación para escribir 8 bits usando mem_write_32
+    {
+        uint64_t address = CURRENT_STATE.REGS[rn] + imm12;
+        uint32_t value = mem_read_32(address);
+        value = (value & 0xFFFFFF00) | (CURRENT_STATE.REGS[rt] & 0xFF);
+        mem_write_32(address, value);
+    }
+}
+
+void ldur(uint32_t rt, uint32_t rn, uint32_t imm12){
+    NEXT_STATE.REGS[rt] = mem_read_32(CURRENT_STATE.REGS[rn] + imm12);
+}
+
+void ldurb(uint32_t rt, uint32_t rn, uint32_t imm12){
+    // Adaptación para leer 8 bits usando mem_read_32
+    {
+        uint64_t address = CURRENT_STATE.REGS[rn] + imm12;
+        uint32_t value = mem_read_32(address);
+        NEXT_STATE.REGS[rt] = value & 0xFF;
+    }
+}
 
 void process_instruction()
 {
@@ -30,24 +98,23 @@ void process_instruction()
     // Se supone que son los opcodes de cada instrucción y los valores se pueden encontrar en TP1-ARM/ref/DDI0487B_a_armv8_arm.pdf
     switch (opcode) {
         case 0x6A2:  // HLT (Halt)
-            printf("HLT: Halting simulation.\n");
-            RUN_BIT = 0;
+            hlt();
             break;
         
-        case 0x588: // adds
-            NEXT_STATE.REGS[(instruction >> 0) & 0x1F] = CURRENT_STATE.REGS[(instruction >> 5) & 0x1F] + CURRENT_STATE.REGS[(instruction >> 16) & 0x1F];
+        case 0x588: // adds immediate
+            adds_imm((instruction >> 0) & 0x1F, (instruction >> 5) & 0x1F, instruction & 0xFFF);
             break;
 
-        case 0x558: // adds
-            NEXT_STATE.REGS[(instruction >> 0) & 0x1F] = CURRENT_STATE.REGS[(instruction >> 5) & 0x1F] + (instruction & 0x3FF);
+        case 0x558: // adds extended
+            adds_ext((instruction >> 0) & 0x1F, (instruction >> 5) & 0x1F, instruction & 0xFFF);
             break;
 
         case 0x750: // ands
             NEXT_STATE.REGS[(instruction >> 0) & 0x1F] = CURRENT_STATE.REGS[(instruction >> 5) & 0x1F] & CURRENT_STATE.REGS[(instruction >> 16) & 0x1F];
             break;
 
-        case 0x758: //subs
-            NEXT_STATE.REGS[(instruction >> 0) & 0x1F] = CURRENT_STATE.REGS[(instruction >> 5) & 0x1F] - CURRENT_STATE.REGS[(instruction >> 16) & 0x1F];
+        case 0x758: //subs immediate
+            subs_imm((instruction >> 0) & 0x1F, (instruction >> 5) & 0x1F, instruction & 0xFFF);
             break;
 
         case 0x2A0: // beq
@@ -57,46 +124,35 @@ void process_instruction()
             break;
 
         case 0x650: // eors
-            NEXT_STATE.REGS[(instruction >> 0) & 0x1F] = CURRENT_STATE.REGS[(instruction >> 5) & 0x1F] ^ CURRENT_STATE.REGS[(instruction >> 16) & 0x1F];
+            eors((instruction >> 0) & 0x1F, (instruction >> 5) & 0x1F, (instruction >> 16) & 0x1F);
             break;
 
         case 0x694: // movz (creo que mov tambien)
-            NEXT_STATE.REGS[(instruction >> 0) & 0x1F] = (instruction >> 5) & 0xFFFF;
+            movz((instruction >> 0) & 0x1F, instruction & 0xFFFF);
             break;
 
         case 0x69B: // lsl
-            NEXT_STATE.REGS[(instruction >> 0) & 0x1F] = CURRENT_STATE.REGS[(instruction >> 5) & 0x1F] << (instruction & 0x3F);
+            void lsl(uint32_t rd, uint32_t rn, uint32_t shamt);
             break;
 
         case 0x7C0: // stur
-            mem_write_32(CURRENT_STATE.REGS[(instruction >> 5) & 0x1F] + (instruction & 0xFFF), CURRENT_STATE.REGS[(instruction >> 0) & 0x1F]);
+            void stur(uint32_t rt, uint32_t rn, uint32_t imm12);
             break;
         
         case 0x1C0: // sturb
-            // Adaptación para escribir 8 bits usando mem_write_32
-            {
-                uint64_t address = CURRENT_STATE.REGS[(instruction >> 5) & 0x1F] + (instruction & 0xFFF);
-                uint32_t value = mem_read_32(address);
-                value = (value & 0xFFFFFF00) | (CURRENT_STATE.REGS[(instruction >> 0) & 0x1F] & 0xFF);
-                mem_write_32(address, value);
-            }
+            void sturb(uint32_t rt, uint32_t rn, uint32_t imm12);
             break;
 
         case 0x7C2: // ldur
-            NEXT_STATE.REGS[(instruction >> 0) & 0x1F] = mem_read_32(CURRENT_STATE.REGS[(instruction >> 5) & 0x1F] + (instruction & 0xFFF));
+            void ldur(uint32_t rt, uint32_t rn, uint32_t imm12);
             break;
 
         case 0x1C2: // ldurb
-            // Adaptación para leer 8 bits usando mem_read_32
-            {
-                uint64_t address = CURRENT_STATE.REGS[(instruction >> 5) & 0x1F] + (instruction & 0xFFF);
-                uint32_t value = mem_read_32(address);
-                NEXT_STATE.REGS[(instruction >> 0) & 0x1F] = value & 0xFF;
-            }
+            void ldurb(uint32_t rt, uint32_t rn, uint32_t imm12);
             break;
         
-        case 0x788: // subs
-            NEXT_STATE.REGS[(instruction >> 0) & 0x1F] = CURRENT_STATE.REGS[(instruction >> 5) & 0x1F] - (instruction & 0xFFF);
+        case 0x788: // subs extended
+            subs_ext((instruction >> 0) & 0x1F, (instruction >> 5) & 0x1F, instruction & 0xFFF);
             break;
 
         default:
