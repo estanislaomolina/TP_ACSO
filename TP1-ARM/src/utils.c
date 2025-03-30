097814280b75d9d7f3946a27a5de04c983a2f6ec
estanislaomolina <estanislaomolina05@gmail.com>
2025-03-30 20:23:46 -0300

    refactor utils.c and utils.h: improve code formatting and consistency

--- TP1-ARM/src/utils.c
+++ TP1-ARM/src/utils.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 #include <stdint.h>
-
 #include "utils.h"
 
-
-void print_binary(uint32_t num) {
-    for (int i = 31; i >= 0; i--) {
-        printf("%d", (num >> i) & 1); // Extract each bit and print it
+void print_binary(uint32_t num)
+{
+    for (int i = 31; i >= 0; i--)
+    {
+        printf("%d", (num >> i) & 1);
     }
     printf("\n");
 }
 
-uint32_t extract_bits(uint32_t num, uint8_t start, uint8_t end) {
+uint32_t extract_bits(uint32_t num, uint8_t start, uint8_t end)
+{
     uint32_t mask = 0;
-    for (int i = start; i <= end; i++) {
+    for (int i = start; i <= end; i++)
+    {
         mask |= 1 << i;
     }
     return (num & mask) >> start;
 }
 
-int64_t sign_extend(uint32_t number, int bits) {
-    //if number is negative extends with ones, else extends with zeros 
-    if ((number >> (bits - 1))) {
-      number |= 0xFFFFFFFFFFFFFFFF << bits;
+int64_t sign_extend(uint32_t number, int bits)
+{
+    if ((number >> (bits - 1)))
+    {
+        number |= 0xFFFFFFFFFFFFFFFF << bits;
     }
-  
+
     return number;
-  }
+}
--- TP1-ARM/src/utils.h
+++ TP1-ARM/src/utils.h
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
 
-
-
 void print_binary(uint32_t num);
 uint32_t extract_bits(uint32_t num, uint8_t start, uint8_t end);
 int64_t sign_extend(uint32_t value, int bits);
