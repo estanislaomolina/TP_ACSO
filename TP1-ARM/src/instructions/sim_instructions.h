5e8ea5315922bcfb13677236f59172faf3f9bf75
estanislaomolina <estanislaomolina05@gmail.com>
2025-03-30 20:25:46 -0300

    refactor sim_instructions.h: improve code formatting and remove comments

--- TP1-ARM/src/instructions/sim_instructions.h
+++ TP1-ARM/src/instructions/sim_instructions.h
@@ -3,47 +3,47 @@
 #ifndef SIM_INSTRUCTIONS_H
 #define SIM_INSTRUCTIONS_H
 
-void hlt(); // FUNCIONAN
-void adds_imm(uint32_t instr); // FUNCIONAN
-void adds_ext(uint32_t instr); // FUNCIONAN
-void subs_imm(uint32_t instr); // FUNCIONAN
-void subs_ext(uint32_t instr); // FUNCIONAN
-void ands(uint32_t instr); // FUNCIONAN
-void eors(uint32_t instr); // FUNCIONAN
+void hlt(); 
+void adds_imm(uint32_t instr); 
+void adds_ext(uint32_t instr); 
+void subs_imm(uint32_t instr); 
+void subs_ext(uint32_t instr); 
+void ands(uint32_t instr); 
+void eors(uint32_t instr); 
 
 
-void orr(uint32_t instr); // FUNCIONAN
-void b(uint32_t instr); //funciona
-void br(uint32_t instr); //revisar
-void cmp(uint32_t instr); //FUNCIONAN
-void beq(uint32_t instr); //FUNCIONAN
-void bne(uint32_t instr); //FUNCIONAN
-void bgt(uint32_t instr); //FUNCIONAN
-void bge(uint32_t instr); //FUNCIONAN
-void blt(uint32_t instr); //FUNCIONAN
-void ble(uint32_t instr); //FUNCIONAN
-void bcond(uint32_t instr); //FUNCIONAN
+void orr(uint32_t instr); 
+void b(uint32_t instr); 
+void br(uint32_t instr); 
+void cmp(uint32_t instr); 
+void beq(uint32_t instr); 
+void bne(uint32_t instr); 
+void bgt(uint32_t instr); 
+void bge(uint32_t instr); 
+void blt(uint32_t instr);
+void ble(uint32_t instr); 
+void bcond(uint32_t instr); 
 
-void movz(uint32_t instr); // FUNCIONAN
+void movz(uint32_t instr); 
 
-void cbz(uint32_t instr); //FUNCIONA
-void cbnz(uint32_t instr); //FUNCIONA
-void mul(uint32_t instr); //FUNCIONA
+void cbz(uint32_t instr); 
+void cbnz(uint32_t instr); 
+void mul(uint32_t instr); 
 
 
-void lsl(uint32_t instr); // FUNCIONAN
-void lsr(uint32_t instr); // FUNCIONAN
-void shifts(uint32_t instr); // FUNCIONAN
-void stur(uint32_t instr); // FUNCIONAN
-void sturb(uint32_t instr); // FUNCIONAN
-void sturh(uint32_t instr); // FUNCIONAN
-void ldur(uint32_t instr); // FUNCIONAN
-void ldurb(uint32_t instr); // FUNCIONAN
-void ldurh(uint32_t instr); // FUNCIONAN
-void mov(uint32_t instr); // FUNCIONAN
+void lsl(uint32_t instr); 
+void lsr(uint32_t instr); 
+void shifts(uint32_t instr); 
+void stur(uint32_t instr); 
+void sturb(uint32_t instr); 
+void sturh(uint32_t instr); 
+void ldur(uint32_t instr); 
+void ldurb(uint32_t instr); 
+void ldurh(uint32_t instr); 
+void mov(uint32_t instr); 
 
-void add_imm(uint32_t instr); // FUNCIONAN
-void add_ext(uint32_t instr); // FUNCIONAN
+void add_imm(uint32_t instr); 
+void add_ext(uint32_t instr); 
 
 
 #endif
