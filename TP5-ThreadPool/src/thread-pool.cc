18b277856b0ae1d3d59196e0f5f2235e29fb1c77
estanislaomolina <estanislaomolina05@gmail.com>
2025-06-14 19:38:42 -0300

    defino worker y dispatcher tal como estan en el .h

--- TP5-ThreadPool/src/thread-pool.cc
+++ TP5-ThreadPool/src/thread-pool.cc
@@ -7,11 +7,29 @@
 #include "thread-pool.h"
 using namespace std;
 
-ThreadPool::ThreadPool(size_t numThreads) : wts(numThreads), done(false) {}
+ThreadPool::ThreadPool(size_t numThreads) : wts(numThreads), done(false) {
+    // Constructor que configura el Threadpool para crear un número especificado de hilos.
+}
 
 
-void ThreadPool::schedule(const function<void(void)>& thunk) {}
+void ThreadPool::schedule(const function<void(void)>& thunk) {
+    // Programa el thunk proporcionado para ser ejecutado por alguno de los hilos del ThreadPool
+    // tan pronto como todos los thunks previamente programados hayan sido manejados.
+}
 
-void ThreadPool::wait() {}
 
-ThreadPool::~ThreadPool() {}
+void ThreadPool::wait() {
+    // Bloquea y espera hasta que todos los thunks previamente programados hayan sido ejecutados completamente
+}
+
+void ThreadPool::worker(int id){}
+
+void ThreadPool::dispatcher(){}
+
+
+
+ThreadPool::~ThreadPool() {
+    //  Destructor que espera a que todos los thunks programados se ejecuten y luego cierra 
+    // correctamente el ThreadPool y cualquier recurso utilizado durante su vida útil.
+
+}
