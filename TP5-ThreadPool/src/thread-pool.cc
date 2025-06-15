5c87fc543d791cabaeb31bd11d399908d77a3e7c
estanislaomolina <estanislaomolina05@gmail.com>
2025-06-14 22:17:51 -0300

    worker y dispatcher

--- TP5-ThreadPool/src/thread-pool.cc
+++ TP5-ThreadPool/src/thread-pool.cc
@@ -46,11 +46,55 @@ void ThreadPool::wait() {
 }
 
 void ThreadPool::worker(int id){
-    return;
+    // definicion del worker con sus distintas tareas
+    while (true) {
+        idWorker_disponibles.wait(); // espera a que haya un worker disponible
+        idWorkerLock.lock(); // bloqueo el mutex para proteger el acceso a la queue de workers disponibles
+        if (done && tareas.empty()) {
+            idWorkerLock.unlock(); // desbloqueo el mutex
+            return; // si se ha marcado como terminado y no hay tareas, salgo del worker
+        }
+        int workerId = idWorker.front(); // obtengo el id del worker disponible
+        idWorker.pop(); // saco el worker de la queue
+        idWorkerLock.unlock(); // desbloqueo el mutex
+
+        function<void(void)> tarea;
+        tareas_disponibles.wait(); // espero a que haya tareas disponibles
+
+        {
+            std::lock_guard<mutex> lock(queueLock); // bloqueo el mutex para proteger el acceso a la queue de tareas
+            if (!tareas.empty()) {
+                tarea = tareas.front(); // obtengo la tarea del frente de la queue
+                tareas.pop(); // saco la tarea de la queue
+            }
+        }
+
+        if (tarea) { // si hay una tarea, la ejecuto
+            tarea();
+            wts[workerId].sem.signal(); // despierto al worker correspondiente
+            std::lock_guard<std::mutex> lock(completionMutex); // bloqueo el mutex para proteger el acceso a las variables de finalización
+            tareasCompletadas++; // incremento el contador de tareas completadas
+            completionCv.notify_all(); // notifico a todos los que están esperando en wait()
+        }
+    }
 }
 
 void ThreadPool::dispatcher(){
-    return;
+    // defino el dispatcher que se encarga de asignar las tareas a los workers disponbles
+    while (true) {
+        tareas_disponibles.wait(); // espera a que haya tareas disponibles
+        if (done && tareas.empty()) {
+            return; // si se ha marcado como terminado y no hay tareas, salgo del dispatcher
+        }
+
+        idWorker_disponibles.wait(); // espera a que haya un worker disponible
+        idWorkerLock.lock(); // bloqueo el mutex para proteger el acceso a la queue de workers disponibles
+        int workerId = idWorker.front(); // obtengo el id del worker disponible
+        idWorker.pop(); // saco el worker de la queue
+        idWorkerLock.unlock(); // desbloqueo el mutex
+
+        wts[workerId].sem.signal(); // despierto al worker correspondiente
+    }
 }
 
 
