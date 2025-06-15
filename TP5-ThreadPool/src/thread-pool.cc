578f139cc80f26c202d6a5a1e58ab56e874c5258
estanislaomolina <estanislaomolina05@gmail.com>
2025-06-14 23:54:00 -0300

    creo que listo. quiero hacer mas tests igual

--- TP5-ThreadPool/src/thread-pool.cc
+++ TP5-ThreadPool/src/thread-pool.cc
@@ -5,123 +5,150 @@
  */
 
 #include "thread-pool.h"
+#include <stdexcept>
 using namespace std; // std:: es el namespace donde se encuentra la librería estándar de C++
 
-
-ThreadPool::ThreadPool(size_t numThreads) : wts(numThreads), done(false) {
+ThreadPool::ThreadPool(size_t numThreads) : wts(numThreads), done(false)
+{
     // Constructor que configura el Threadpool para crear un número especificado de hilos.
-    for (size_t i = 0; i < numThreads; i++ ) {
-        idWorker.push(i); // inicializo el vector de hilos disponibles
+    for (size_t i = 0; i < numThreads; i++)
+    {
+        idWorker.push(i);              // inicializo el vector de hilos disponibles
         idWorker_disponibles.signal(); // incremento el semaphore de workers
     }
     // Inicializo los hilos de trabajo
-    for (size_t i = 0; i < numThreads; i++) {
-        wts[i].idWorker = i; // asigno el idWorker al worker
+    for (size_t i = 0; i < numThreads; i++)
+    {
+        wts[i].idWorker = i;                              // asigno el idWorker al worker
         wts[i].ts = thread(&ThreadPool::worker, this, i); // inicio el worker con su id
-}
+    }
     dt = thread(&ThreadPool::dispatcher, this); // inicio el dispatcher
 }
 
-
-void ThreadPool::schedule(const function<void(void)>& thunk) {
+void ThreadPool::schedule(const function<void(void)> &thunk)
+{
     // Programa el thunk proporcionado para ser ejecutado por alguno de los hilos del ThreadPool
     // tan pronto como todos los thunks previamente programados hayan sido manejados.
+    if (done)
+    {
+        throw runtime_error("El ThreadPool ya ha sido destruido."); // lanzo una excepción si el ThreadPool ya ha sido destruido
+    }
+    if (!thunk)
+    {
+        throw std::invalid_argument("El thunk no puede ser nulo."); // lanzo una excepción si el thunk es nulo
+    }
     {
         std::lock_guard<mutex> lock(queueLock); // bloqueo el mutex para proteger el acceso a la queue de tareas
-        tareas.push(thunk); // agrego el thunk a la queue de tareas
+        tareas.push(thunk);                     // agrego el thunk a la queue de tareas
     }
 
     {
-    std::lock_guard<mutex> lock(completionMutex); // bloqueo el mutex para proteger el acceso a las variables de finalización
-    totalTareas++; // incremento el contador de tareas totales
+        std::lock_guard<mutex> lock(completionMutex); // bloqueo el mutex para proteger el acceso a las variables de finalización
+        totalTareas++;                                // incremento el contador de tareas totales
     }
     tareas_disponibles.signal(); // incremento el semaphore de tareas disponibles
 }
 
-void ThreadPool::wait() {
+void ThreadPool::wait()
+{
     // Bloquea y espera hasta que todos los thunks previamente programados hayan sido ejecutados completamente
     std::unique_lock<std::mutex> lock(completionMutex); // bloqueo el mutex para proteger el acceso a las variables de finalización
-    completionCv.wait(lock, [this]() { // 
-      return tareasCompletadas == totalTareas;
+    completionCv.wait(lock, [this]() {                  //
+        return tareasCompletadas == totalTareas;
     });
 }
 
-void ThreadPool::worker(int id){
+void ThreadPool::worker(int id)
+{
     // definicion del worker con sus distintas tareas
-    while (true) {
+    while (true)
+    {
         wts[id].sem.wait(); // espero a que haya una tarea disponible
-        if (done) {
+        if (done)
+        {
             return; // si se ha marcado como terminado, salgo del worker
         }
         wts[id].thunk(); // ejecuto la tarea
         {
             std::lock_guard<std::mutex> lock(idWorkerLock); // bloqueo el mutex para proteger el acceso a las variables de finalización
-            idWorker.push(id); // agrego el id del worker a la queue de workers disponibles
+            idWorker.push(id);                              // agrego el id del worker a la queue de workers disponibles
         }
         idWorker_disponibles.signal(); // incremento el semaphore de workers
         {
             std::lock_guard<std::mutex> lock(completionMutex); // bloqueo el mutex para proteger el acceso a las variables de finalización
-            tareasCompletadas++; // incremento el contador de tareas completadas
-            if (tareasCompletadas == totalTareas) {
+            tareasCompletadas++;                               // incremento el contador de tareas completadas
+            if (tareasCompletadas == totalTareas)
+            {
                 completionCv.notify_all(); // notifico a todos los que están esperando que todas las tareas se han completado
             }
         }
     }
 }
 
-void ThreadPool::dispatcher(){
+void ThreadPool::dispatcher()
+{
     // defino el dispatcher que se encarga de asignar las tareas a los workers disponbles
-    while (true) {
+    while (true)
+    {
         tareas_disponibles.wait(); // espero a que haya tareas disponibles
         {
             std::lock_guard<std::mutex> lock(queueLock); // bloqueo el mutex para proteger el acceso a la queue de tareas
-            if (done && tareas.empty()) {
+            if (done && tareas.empty())
+            {
                 return; // si se ha marcado como terminado y no hay tareas, salgo del dispatcher
             }
         }
         function<void(void)> tarea;
         {
             std::lock_guard<std::mutex> lock(queueLock); // bloqueo el mutex para proteger el acceso a la queue de tareas
-            if (!tareas.empty()) {
+            if (!tareas.empty())
+            {
                 tarea = tareas.front(); // obtengo la tarea del frente de la queue
-                tareas.pop(); // saco la tarea de la queue
+                tareas.pop();           // saco la tarea de la queue
             }
         }
         idWorker_disponibles.wait(); // espero a que haya un worker disponible
         int workerId;
         {
             std::lock_guard<std::mutex> lock(idWorkerLock); // bloqueo el mutex para proteger el acceso a la queue de workers disponibles
-            if (!idWorker.empty()) {
+            if (!idWorker.empty())
+            {
                 workerId = idWorker.front(); // obtengo el id del worker disponible
-                idWorker.pop(); // saco el worker de la queue
-            } else {
+                idWorker.pop();              // saco el worker de la queue
+            }
+            else
+            {
                 continue; // si no hay workers disponibles, sigo esperando
             }
         }
-        if (tarea) { // si hay una tarea, la asigno al worker
-            wts[workerId].thunk = tarea; // asigno la tarea al worker
+        if (tarea)
+        {                                    // si hay una tarea, la asigno al worker
+            wts[workerId].thunk = tarea;     // asigno la tarea al worker
             wts[workerId].available = false; // marco el worker como no disponible
-            wts[workerId].sem.signal(); // despierto al worker para que ejecute la tarea
+            wts[workerId].sem.signal();      // despierto al worker para que ejecute la tarea
         }
     }
 }
 
-
-
-ThreadPool::~ThreadPool() {
-    //  Destructor que espera a que todos los thunks programados se ejecuten y luego cierra 
+ThreadPool::~ThreadPool()
+{
+    //  Destructor que espera a que todos los thunks programados se ejecuten y luego cierra
     // correctamente el ThreadPool y cualquier recurso utilizado durante su vida útil.
-    wait(); // espero a que todas las tareas se completen
-    done = true; // marco el ThreadPool como terminado
+    wait();                      // espero a que todas las tareas se completen
+    done = true;                 // marco el ThreadPool como terminado
     tareas_disponibles.signal(); // despierto al dispatcher para que salga del bucle
-    if (dt.joinable()) {
+    if (dt.joinable())
+    {
         dt.join(); // espero a que el dispatcher termine
     }
-    for (size_t i = 0; i < wts.size(); i++) {
+    for (size_t i = 0; i < wts.size(); i++)
+    {
         wts[i].sem.signal(); // despierto al worker para que salga del bucle
     }
-    for (size_t i = 0; i < wts.size(); i++) {
-        if (wts[i].ts.joinable()) {
+    for (size_t i = 0; i < wts.size(); i++)
+    {
+        if (wts[i].ts.joinable())
+        {
             wts[i].ts.join(); // espero a que el worker termine
         }
     }
--- TP5-ThreadPool/src/thread-pool.h
+++ TP5-ThreadPool/src/thread-pool.h
@@ -10,102 +10,101 @@
 #ifndef _thread_pool_
 #define _thread_pool_
 
-#include <cstddef>     // for size_t
-#include <functional>  // for the function template used in the schedule signature
-#include <thread>      // for thread
-#include <vector>      // for vector
-#include "Semaphore.h" // for Semaphore
-#include <queue>       // para queue
-#include <mutex>       // para mutex
-#include <condition_variable>  // para wait()
+#include <cstddef>            // for size_t
+#include <functional>         // for the function template used in the schedule signature
+#include <thread>             // for thread
+#include <vector>             // for vector
+#include "Semaphore.h"        // for Semaphore
+#include <queue>              // para queue
+#include <mutex>              // para mutex
+#include <condition_variable> // para wait()
 
 using namespace std;
 
-
 /**
  * @brief Represents a worker in the thread pool.
- * 
- * The `worker_t` struct contains information about a worker 
- * thread in the thread pool. Should be includes the thread object, 
- * availability status, the task to be executed, and a semaphore 
- * (or condition variable) to signal when work is ready for the 
+ *
+ * The `worker_t` struct contains information about a worker
+ * thread in the thread pool. Should be includes the thread object,
+ * availability status, the task to be executed, and a semaphore
+ * (or condition variable) to signal when work is ready for the
  * worker to process.
  */
-typedef struct worker {
-    thread ts;
-    function<void(void)> thunk;
-    /**
-     * Complete the definition of the worker_t struct here...
-     **/
-    Semaphore sem{0}; // señal para despertar al worker. esta con "{0}" para poder inicializarlo con ese valor.
-    bool available = true; // marca si el worker está libre
-    int idWorker = -1; // identificador del worker
+typedef struct worker
+{
+  thread ts;
+  function<void(void)> thunk;
+  /**
+   * Complete the definition of the worker_t struct here...
+   **/
+  Semaphore sem{0};      // señal para despertar al worker. esta con "{0}" para poder inicializarlo con ese valor.
+  bool available = true; // marca si el worker está libre
+  int idWorker = -1;     // identificador del worker
 } worker_t;
 
-class ThreadPool {
-  public:
+class ThreadPool
+{
+public:
+  /**
+   * Constructs a ThreadPool configured to spawn up to the specified
+   * number of threads.
+   */
+  ThreadPool(size_t numThreads);
 
   /**
-  * Constructs a ThreadPool configured to spawn up to the specified
-  * number of threads.
-  */
-    ThreadPool(size_t numThreads);
+   * Schedules the provided thunk (which is something that can
+   * be invoked as a zero-argument function without a return value)
+   * to be executed by one of the ThreadPool's threads as soon as
+   * all previously scheduled thunks have been handled.
+   */
+  void schedule(const function<void(void)> &thunk);
 
   /**
-  * Schedules the provided thunk (which is something that can
-  * be invoked as a zero-argument function without a return value)
-  * to be executed by one of the ThreadPool's threads as soon as
-  * all previously scheduled thunks have been handled.
-  */
-    void schedule(const function<void(void)>& thunk);
+   * Blocks and waits until all previously scheduled thunks
+   * have been executed in full.
+   */
+  void wait();
 
   /**
-  * Blocks and waits until all previously scheduled thunks
-  * have been executed in full.
-  */
-    void wait();
+   * Waits for all previously scheduled thunks to execute, and then
+   * properly brings down the ThreadPool and any resources tapped
+   * over the course of its lifetime.
+   */
+  ~ThreadPool();
 
-  /**
-  * Waits for all previously scheduled thunks to execute, and then
-  * properly brings down the ThreadPool and any resources tapped
-  * over the course of its lifetime.
-  */
-    ~ThreadPool();
-    
-  private:
+private:
+  void worker(int id);
+  void dispatcher();
+  thread dt;            // dispatcher thread handle
+  vector<worker_t> wts; // worker thread handles. you may want to change/remove this
+  bool done;            // flag to indicate the pool is being destroyed
+  mutex queueLock;      // mutex to protect the queue of tasks
 
-    void worker(int id);
-    void dispatcher();
-    thread dt;                              // dispatcher thread handle
-    vector<worker_t> wts;                   // worker thread handles. you may want to change/remove this
-    bool done;                              // flag to indicate the pool is being destroyed
-    mutex queueLock;                        // mutex to protect the queue of tasks
+  /* It is incomplete, there should be more private variables to manage the structures...
+   * *
+   */
+  queue<function<void(void)>> tareas; // queue de tareas a ejecutar
+  Semaphore tareas_disponibles{0};    // semaphore para indicar cuantas tareas hay
 
-    /* It is incomplete, there should be more private variables to manage the structures... 
-    * *
-    */
-   queue<function<void(void)>> tareas; // queue de tareas a ejecutar
-   Semaphore tareas_disponibles{0}; // semaphore para indicar cuantas tareas hay
+  queue<int> idWorker;               // queue de workers disponibles segun su id
+  Semaphore idWorker_disponibles{0}; // semaphore para indicar cuantas workers hay
 
-   queue<int> idWorker; // queue de workers disponibles segun su id
-   Semaphore idWorker_disponibles{0}; // semaphore para indicar cuantas workers hay
+  mutex idWorkerLock; // mutex para proteger el acceso a la queue de workers disponibles
 
-   mutex idWorkerLock; // mutex para proteger el acceso a la queue de workers disponibles
+  // para wait()
+  mutex completionMutex;           // mutex para proteger el acceso a las variables de finalización
+  condition_variable completionCv; // espera a que todos terminen
+  size_t totalTareas = 0;          // cuantas tareas se han scheduleado
+  size_t tareasCompletadas = 0;    // cuantas ya finalizaron
 
-   // para wait()
-   mutex completionMutex; // mutex para proteger el acceso a las variables de finalización
-   condition_variable completionCv; // espera a que todos terminen
-   size_t totalTareas = 0; // cuantas tareas se han scheduleado
-   size_t tareasCompletadas = 0; // cuantas ya finalizaron
-
-   /* ThreadPools are the type of thing that shouldn't be cloneable, since it's
-    * not clear what it means to clone a ThreadPool (should copies of all outstanding
-    * functions to be executed be copied?).
-    *
-    * In order to prevent cloning, we remove the copy constructor and the
-    * assignment operator.  By doing so, the compiler will ensure we never clone
-    * a ThreadPool. */
-    ThreadPool(const ThreadPool& original) = delete;
-    ThreadPool& operator=(const ThreadPool& rhs) = delete;
+  /* ThreadPools are the type of thing that shouldn't be cloneable, since it's
+   * not clear what it means to clone a ThreadPool (should copies of all outstanding
+   * functions to be executed be copied?).
+   *
+   * In order to prevent cloning, we remove the copy constructor and the
+   * assignment operator.  By doing so, the compiler will ensure we never clone
+   * a ThreadPool. */
+  ThreadPool(const ThreadPool &original) = delete;
+  ThreadPool &operator=(const ThreadPool &rhs) = delete;
 };
 #endif
